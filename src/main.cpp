4871a2e7342354609e416133377cad676fc8e508
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2020-04-05 16:38:01 +0200

    not working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
+#include <cndl/Route.h>
+#include <cndl/Server.h>
+#include <simplyfile/socket/Host.h>
 
-#include "sargparse/Parameter.h"
-#include "sargparse/ArgumentParsing.h"
+using namespace std::literals::string_view_literals;
+
+cndl::GlobalRoute a_route {R"(/(.+)/?)", [](cndl::Request const&, std::string_view arg) -> cndl::OptResponse {
+    // arg will have the value of the capture in the regex
+    return "<html><head></head><body><h1>cndl rocks</h1></body></html>"sv;
+}, {.methods={"GET", "POST"}}};
+
+struct : cndl::WebsocketHandler {
+    using Websocket = cndl::Websocket;
+    using Request = cndl::Request;
+    void onMessage([[maybe_unused]] Websocket& ws, [[maybe_unused]] AnyMessage msg) override {
+        if (std::holds_alternative<TextMessage>(msg)) {
+            std::cout << std::get<TextMessage>(msg) << std::endl;
+        }
+        ws.send(msg);
+    }
+
+    bool onOpen([[maybe_unused]] Request const& request, [[maybe_unused]] Websocket& ws, [[maybe_unused]] int magic) {
+        std::cout << "socket connected " << request.header.url << std::endl;
+        return true;
+    }
+
+    void onClose([[maybe_unused]] Websocket& ws) override {
+        std::cout << "socket closed" << std::endl;
+    }
+} echo_handler{};
 
 
-namespace
+int main()
 {
 
-sargp::Parameter<std::optional<std::string>> printHelp{{}, "help", "print this help add a string which will be used in a grep-like search through the parameters"};
+    cndl::Server& server = cndl::Server::getGlobalServer();
+    server.listen(simplyfile::getHosts("localhost", "8080")); // listen on localhost (ipv4 and ipv6 if available)
 
-}
+    cndl::WSRoute wsroute{std::regex{R"(/(\d+)/)"}, echo_handler}; // route requests to /[number]/ to the echo_handler (onConnect will be called with the value of [number])
+    server.getDispatcher().addRoute(&wsroute); // hook the route to the server (you can have multiple routes fro a single endpoint) 
 
-int main(int argc, char** argv) {
-	if (std::string(argv[argc-1]) == "--compgen") {
-		std::cout << sargp::compgen(argc-2, argv+1);
-		return 0;
+	while(true) {
+		auto work = server.wait();
+		std::cout << "work: " << work.size() << "\n";
+		server.dispatch(work);
 	}
-    
-	// pass everything except the name of the application
-	sargp::parseArguments(argc-1, argv+1);
-
-	if (printHelp) {
-		std::cout << sargp::generateHelpString(std::regex{".*" + printHelp.get().value_or("") + ".*"});
-		return 0;
-	}
-
-	sargp::callCommands();
-
     return 0;
 }
+
